6d42b24cce29ee76f6cdcdc2df1c9d691856d02f
Estri-Nimsi <n.l.i.rabota@gmail.com>
2026-09-02 22:27:02 +0300

    02.09 22/26

--- Пробные файлы/js1.js
+++ Пробные файлы/js1.js
@@ -1,17 +1,17 @@
 "use strict";
 
-let arr = [];
-
-function func1(num1) {
-	return function func2(num2) {
-		return function func3(num3) {
-			return function func4(num4) {
-				return function func5(num5) {
-					arr.push(num1, num2, num3, num4, num5);
-				}
-			}
-		};
-	};
+function each(arr, callback) {
+	let result = [];
+	
+	for (let elem of arr) {
+		result.push( callback(elem) ); // вызываем функцию-коллбэк
+	}
+	
+	return result;
 }
 
-console.log(func1(2)(3)(4)(5)());
+let result = each([1, 2, 3, 4, 5], function(num) {
+	return num ** 2;
+});
+
+console.log(result);
