ecee41e085b85b29f26d6ccbcac2a9d0767c027d
Estri-Nimsi <n.l.i.rabota@gmail.com>
2026-09-02 18:01:01 +0300

    02.09 18/00

--- Пробные файлы/js1.js
+++ Пробные файлы/js1.js
@@ -1,12 +1,17 @@
 "use strict";
 
-function test(num) {
-	function func(localNum) {
-		num = 2;
-	}
-	
-	func(num);
-	console.log(num);
+let arr = [];
+
+function func1(num1) {
+	return function func2(num2) {
+		return function func3(num3) {
+			return function func4(num4) {
+				return function func5(num5) {
+					arr.push(num1, num2, num3, num4, num5);
+				}
+			}
+		};
+	};
 }
 
-test(1);
+console.log(func1(2)(3)(4)(5)());
