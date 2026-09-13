0f78d9fb9cba6e663e22990108a3feaa1c806ebe
Estri-Nimsi <n.l.i.rabota@gmail.com>
2026-09-13 23:11:49 +0300

    13.09 23/11

--- Пробные файлы/js1.js
+++ Пробные файлы/js1.js
@@ -1,4 +1,8 @@
 "use strict";
 
-let arr = [...'12345'];
-console.log(arr);
+function func(...nums) {
+    return nums.reduce((acc, elem) => acc + elem, 0) / nums.length
+}
+
+let result = func(1, 2, 3);
+console.log(result);
